4045c4a18ac7d8fab72126c7b3a3fb33c3f20820
Gabriel Brown <gabriel.brown.h@gmail.com>
2023-12-10 21:43:12 -0500

    day 10 part 1 success

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -23,19 +23,21 @@ object Day10 {
 
   def part1: IO[Int] = {
 
-    // Approach: farthest away from animal is 1/2 of total pipe length. Count up all pipes connected to starting loop, then reduce by half.
+    // Approach: farthest away from animal is 1/2 of total pipe length, rounded up. Count up all pipes connected to starting loop, then reduce by half.
 
 
     def totalPipes(grid: List[List[Char]]): Int = {
 
+      val reversedGrid = grid.reverse
+
       // Find starting position
-      val indexedGrid = grid.map(_.zipWithIndex).zipWithIndex
+      val indexedGrid = reversedGrid.map(_.zipWithIndex).zipWithIndex
 
       val startPos = indexedGrid.collect {
         case (row, y) if row.exists(_._1 == 'S') =>
           val x = row.find(_._1 == 'S').get._2
           (x, y)
-      }
+      }.head
 
       // Track the pipe
 
@@ -51,8 +53,10 @@ object Day10 {
         val currentPoint = pipeVector.position
         val direction = pipeVector.direction
 
-        val currentPointChar = grid(currentPoint.y)(currentPoint.x)
+        val currentPointChar = reversedGrid(currentPoint.y)(currentPoint.x)
 
+        // TODO: may need to reverse y changes to account for the fact that they're indices, not actual points
+        // Alternatively, reverse the outer list of lines
         (currentPointChar, direction) match {
           case ('|', Direction.North) => PipeVector(
             Point(currentPoint.x, currentPoint.y + 1),
@@ -107,7 +111,7 @@ object Day10 {
 
       @tailrec
       def followPipe(pipeVector: PipeVector, steps: Int): Int =
-        if (grid(pipeVector.position.y)(pipeVector.position.x) == 'S') steps
+        if (reversedGrid(pipeVector.position.y)(pipeVector.position.x) == 'S') steps
         else
           followPipe(
             getNextPointAndDirection(pipeVector),
@@ -115,12 +119,50 @@ object Day10 {
           )
 
       // TODO: find first pipe connecting from start position, then follow pipe and count length, then cut in half
+
+      val firstPipeVector = {
+        /*
+        | is a vertical pipe connecting north and south.
+        - is a horizontal pipe connecting east and west.
+        L is a 90-degree bend connecting north and east.
+        J is a 90-degree bend connecting north and west.
+        7 is a 90-degree bend connecting south and west.
+        F is a 90-degree bend connecting south and east.
+         */
+        val above = reversedGrid(startPos._2 + 1)(startPos._1)
+        val right = reversedGrid(startPos._2)(startPos._1 + 1)
+        val below = reversedGrid(startPos._2 - 1)(startPos._1)
+
+        if (above == '|' || above == '7' || above == 'F')
+          PipeVector(
+            Point(x = startPos._1, y = startPos._2 + 1),
+            Direction.North
+          )
+        else if (right == '-' || right == 'J' || right == '7')
+          PipeVector(
+            Point(x = startPos._1 + 1, y = startPos._2),
+            Direction.East
+          )
+        else if (below == '|' || below == 'L' || below == 'J')
+          PipeVector(
+            Point(x = startPos._1, y = startPos._2 - 1),
+            Direction.South
+          )
+        else
+          PipeVector(
+            Point(x = startPos._1 - 1, y = startPos._2),
+            Direction.West
+          )
+      }
+
+      followPipe(firstPipeVector, 1)
     }
 
     for {
       _ <- IO.println("Day10 part 1: calculating...")
-      lines <- Input.loadAll[IO]("Day10-sample.txt")
-    } yield 0
+      lines <- Input.loadAll[IO]("Day10.txt")
+      totalPipeLength = totalPipes(lines.map(_.toCharArray.toList))
+    } yield Math.ceil(totalPipeLength / 2).toInt
   }
 
   def part2: IO[Int] = {
